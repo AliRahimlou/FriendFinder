5b28e4135b1eecd75081d098daca0c2dc924fdcb
Ali Rahimlou <Rahimlou91@gmail.com>
2019-11-17 16:20:57 -0500

    working stuffs

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Michael",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg/revision/latest?cb=20170701090332",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Jim",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/9/9a/Jim.jpg/revision/latest?cb=20170701084550",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Dwight",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/c/c5/Dwight_.jpg/revision/latest?cb=20170701082424",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Pam",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/6/67/Pam_Beesley.jpg/revision/latest?cb=20170701084358",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Andy",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/b/b5/Andy_Bernard.jpg/revision/latest?cb=20170701084205",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
