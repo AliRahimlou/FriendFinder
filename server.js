94aa87fefaddd18613b931c8e166faea51c69120
Ali Rahimlou <Rahimlou91@gmail.com>
2019-11-17 16:36:16 -0500

    fixed css path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.static(__dirname + "/app/public/css"))
+app.use(express.static(__dirname + "/app/public"))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
